0e19deed34246228e3a511579ce3e2f1bb9d8164
cpbeamer <77639980+cpbeamer@users.noreply.github.com>
2021-03-26 01:03:51 -0400

    Update OrderFlowPivot.cs

--- OrderFlowPivot.cs
+++ OrderFlowPivot.cs
@@ -42,43 +42,22 @@ namespace NinjaTrader.NinjaScript.Strategies
 		private double priorVAH     = 0;
 		
 		// Set up the order, traget, and stop variables
-		private Order entry1 = null;
-		private Order entry2 = null;
-		private Order entry3 = null;
-		private Order entry4 = null;
-		private Order stop1 = null;
-		private Order stop2 = null;
-		private Order stop3 = null;
-		private Order stop4 = null;
-		private Order target1 = null;
-		private Order target2 = null;
-		private Order target3 = null;
-		private Order target4 = null;
+		private Order entryOrder = null;
+		private Order stopOrder = null;
+		private Order targetOrder = null;
 		private int sumFilled = 0;
 		private int barNumberOfOrder = 0;
 		
-		// Arrays used throughout the code
-		private double[] supports = new double[0];
-		private double[] resistances = new double[0];
-		private Order[] allOrders;
-		private Order[] stopOrders;
-		private Order[] targetOrders;
-		
-		//String names for order objects
-		private string[] entryStrings = new String[] { "entry1", "entry2", "entry3", "entry4" };  
-		private string[] stopStrings = new String[] { "stop1", "stop2", "stop3", "stop4" };
-		private string[] targetStrings = new String[] { "target1", "target2", "target3", "target4" };
-		private int[,] stopTargetTicks = new int[,] { {5, 4}, {3, 8}, {3, 10}, {3, 12} }; 		// Int Array of {# of stop ticks, # of target ticks}
-		private string[] allPivots = new string[0];
+		private double[] allPivots;
 		
 		
 		protected override void OnStateChange()
 		{
 			if (State == State.SetDefaults)
 			{
-				Description									= @"Enter the description for your new custom Strategy here.";
+				Description									= @"Automated trading bot that uses Orderflow pivots to scalp";
 				Name										= "OrderFlowPivots";
-				Calculate									= Calculate.OnEachTick;
+				Calculate									= Calculate.OnBarClose;
 				EntriesPerDirection							= 1;
 				EntryHandling								= EntryHandling.AllEntries;
 				IsExitOnSessionCloseStrategy				= true;
@@ -94,155 +73,96 @@ namespace NinjaTrader.NinjaScript.Strategies
 				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
 				BarsRequiredToTrade							= 20;
 				
-				
 				// Disable this property for performance gains in Strategy Analyzer optimizations
 				// See the Help Guide for additional information
 				IsInstantiatedOnEachOptimizationIteration	= true;
+				
 			} 
 			else if (State == State.Configure)
-			{	
+			{
+				
 				//Adds data series for daily charts
 				AddDataSeries("ES 06-21", BarsPeriodType.Day, 1);
 				
-				//Adds data series for 1min charts to cancel unfilled orders 
-				AddDataSeries("ES 06-21", BarsPeriodType.Minute, 1);
-				Print("Data Series Added");
+				double[] allPivots = new double[] { priorHigh, priorLow, priorClose, currentVAL, currentVAL, currentOpen, currentPOC };
 				
+				
+			} 
+			else if (State == State.DataLoaded) 
+			{
 				// Sets the values for the prior day's high, low, and close
 				if (PriorDayOHLC().PriorHigh[1] > 0) {
 				    priorHigh = PriorDayOHLC().PriorHigh[1];
 				    priorLow = PriorDayOHLC().PriorLow[1];
 					priorClose = PriorDayOHLC().PriorClose[1];
 				}
-				Print("Prior HLC complete");
-				//Get data for the current open
-				currentOpen = PriorDayOHLC().Open[1];
 				
 				//Calculate the VAH and VAL of the prior day
 				priorVAH = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).VAt[1];
 				priorVAL = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).VAb[1];
 				Print("prior value area complete");
 				
-				currentVAH = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).VAt[2];
-				currentVAL = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).VAb[2];
-				currentPOC = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).POC[2];
-				Print("current value area complete");
-				
-				allOrders = new Order[] { target1, target2, target3, target4 };
-				stopOrders = new Order[] { stop1, stop2, stop3, stop4 };
-				targetOrders = new Order[] { target1, target2, target3, target4 };
-				Print("new order array complete");
-				
-			} 
+				//Get data for the current open
+				currentOpen = PriorDayOHLC().Open[1];
+			}
 		}
 		
 		protected override void OnBarUpdate() {
 			
-			for (int index = 0; index < allOrders.Length; index++) {
-				
-				/* Cancel all working orders if the price has gone 10 points above our limit order
-				   or if 15 mins have passed since the pacement of our order						*/
-				if (allOrders[index].OrderState == OrderState.Working || GetCurrentBid() > (allOrders[index].LimitPrice + (50 * TickSize))) {
-						CancelOrder(allOrders[index]);
-					Print("on bar update complete");
-				}
-			}
 			
 			
-		}
-		
-		protected override void OnRender(ChartControl chartControl, ChartScale chartScale) {
-		  base.OnRender(chartControl, chartScale);
-		  // loop through only the rendered bars on the chart
-		  for(int barIndex = ChartBars.FromIndex; barIndex <= ChartBars.ToIndex; barIndex++)
-		  {
-		    // get the open price at the selected bar index value
-		    double openPrice = Bars.GetOpen(barIndex);
-			Print("On Render complete");
-		  }
-		}
-		
-		protected void OnTickUpdate() {
+			if (CurrentBars[0] <= BarsRequiredToPlot || CurrentBars[1] <= BarsRequiredToPlot || CurrentBars[2] <= BarsRequiredToPlot)
+        		return;
 			
-			double[] allPivots = new double[] { priorHigh, priorLow, priorClose, currentOpen, currentHigh, currentLow };
-			
-			/* If the current price is greater than the pivot point, add it to the supports array
-			   If the current price is less than the pivot point, add it to the resistance array    */ 
-			for (int index = 0; index < allPivots.Length; index++) {
-				if (allPivots[index] > GetCurrentBid()) {
-					Array.Resize(ref resistances, resistances.Length + 1);
-					resistances[resistances.GetUpperBound(0)] = allPivots[index];
-					Array.Sort(resistances, (x, y) => y.CompareTo(x));
-					Print("resistances complete");
-				} 
-				else if (allPivots[index] < GetCurrentBid()) {
-					Array.Resize(ref supports, supports.Length + 1);
-					supports[supports.GetUpperBound(0)] = allPivots[index];
-					Array.Sort(supports);
-					Print("supports complete");
-			}
-		}
-			
-			/* Loop through the supports array and if the current price is within 12 Ticks (3 points)
+			if (BarsInProgress == 0) {
+				/* Loop through the supports array and if the current price is within 10 Ticks (2.5 points)
 			   of the support price, we are going to place a limit order at the support price and one tick below    */
-			for (int index = 0; index < supports.Length; index++) {
-				if ((GetCurrentBid() - supports[index]) <= (10 * TickSize) && Position.MarketPosition == MarketPosition.Flat) {
-					
-					//Order and Exit Target for first buy
-					EnterLongLimit(1, supports[index], "entry1");
-					//Order and Exit for Second Buy
-					EnterLongLimit(1, supports[index] - TickSize, "entry2");
-					//Order and Exits for Third Buy
-					EnterLongLimit(1, supports[index] - (2 * TickSize), "entry3");
-					EnterLongLimit(1, supports[index] - (2 * TickSize), "entry4");
-					Print("long entries complete");
-				}
-			}
+				for (int index = 0; index < allPivots.Length; index++) {
+					if ((GetCurrentBid() - allPivots[index]) <= (10 * TickSize) && Position.MarketPosition == MarketPosition.Flat) {
+						EnterLongLimit(1, allPivots[index] - TickSize, "entry1");
+					}
+					else if ((allPivots[index] - GetCurrentBid()) >= (10 * TickSize) && Position.MarketPosition == MarketPosition.Flat) {
+						EnterShortLimit(1, allPivots[index] + TickSize, "entry1");
+						}
+					}
 			
-			for (int index = 0; index < supports.Length; index++) {
-				if ((supports[index] - GetCurrentBid()) <= (12 * TickSize) && Position.MarketPosition == MarketPosition.Flat) {
-					//Order and Exit Target for first buy
-					EnterLongLimit(1, resistances[index], "entry1");
-					//Order and Exit for Second Buy
-					EnterLongLimit(1, resistances[index] + TickSize, "entry2");
-					//Order and Exits for Third Buy
-					EnterLongLimit(1, resistances[index] + (2 * TickSize), "entry3");
-					EnterLongLimit(1, resistances[index] + (2 * TickSize), "entry4");
-					Print("short entries complete");
-					
-				}
-			}
-			
-			for (int index = 0; index < stopOrders.Length; index++) {
 				/*  If we have a long position and the current price is 4 ticks in profit, raise the stop loss order to breakeven  */
-				if (Position.MarketPosition == MarketPosition.Long && Close[0] >= Position.AveragePrice + (4 * TickSize)) {	
+				if (Position.MarketPosition == MarketPosition.Long && Close[0] >= Position.AveragePrice + (4 * TickSize) || Position.MarketPosition == MarketPosition.Short && Close[0] >= Position.AveragePrice - (4 * TickSize) ) {	
 					// Checks to see if our Stop order has been submitted already
-		            if (stopOrders[index] != null && stopOrders[index].StopPrice < Position.AveragePrice) {
+		            if (stopOrder != null && stopOrder.StopPrice < Position.AveragePrice) {
 						// Modifies stop-loss to breakeven
-		                stopOrders[index] = ExitLongStopMarket(0, true, stopOrders[index].Quantity, Position.AveragePrice, stopStrings[index], entryStrings[index]);
-						Print("stop order modified");
+		                stopOrder = ExitLongStopMarket(0, true, stopOrder.Quantity, Position.AveragePrice, "MyStop", "entry1");
 					}
 				}
+				
+				currentVAH = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).VAt[0];
+				currentVAL = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).VAb[0];
+				currentPOC = CalculateValueArea(false, @"VWTPO", 0.7, 8, 30, 6.75).POC[0];
+				
+				}
+			
+				else if (BarsInProgress == 1) {
+					
+					currentHigh = High[1];
+					currentLow = Low[1];
+					currentOpen = Open[1];
+				}		
 			}
-		}
+		
 		
 		protected override void OnOrderUpdate(Order order, double limitPrice, double stopPrice, int quantity, int filled, double averageFillPrice, OrderState orderState, DateTime time, ErrorCode error, string nativeError) {
 			
-            // Handle entry orders here. The entry object allows us to identify that the order that is calling the OnOrderUpdate() method is the entry order.
-            // Assign entryOrder in OnOrderUpdate() to ensure the assignment occurs when expected.
-            // This is more reliable than assigning Order objects in OnBarUpdate, as the assignment is not gauranteed to be complete if it is referenced immediately after submitting
-			for (int index = 0; index < allOrders.Length; index++) {
-	            if (order.Name == entryStrings[index]) {
-	                allOrders[index] = order;
-	                // Reset the entryOrder object to null if order was cancelled without any fill
-	                if (order.OrderState == OrderState.Cancelled && order.Filled == 0) {
-	                    allOrders[index] = order;
-	                    sumFilled = 0;
-						Print("Order updated");
-	                }
-	            }
-				
-			}
+	        /* Handle entry orders here. The entry object allows us to identify that the order that is calling the OnOrderUpdate() method is the entry order.
+	           Assign entryOrder in OnOrderUpdate() to ensure the assignment occurs when expected.
+	           is more reliable than assigning Order objects in OnBarUpdate, as the assignment is not gauranteed to be complete if it is referenced immediately after submitting */
+            if (order.Name == "entry1") {
+                entryOrder = order;
+                // Reset the entryOrder object to null if order was cancelled without any fill
+                if (order.OrderState == OrderState.Cancelled && order.Filled == 0) {
+                    entryOrder = order;
+                    sumFilled = 0;
+                }
+            }
 		}
 
 		
@@ -250,29 +170,30 @@ namespace NinjaTrader.NinjaScript.Strategies
 			
 			/* Monitoring OnExecution to trigger the submission of stop/longExit orders instead of OnOrderUpdate() since OnExecution() is called after OnOrderUpdate()
 			   which ensures that the strategy has received the execution which is used for internal signal tracking      */
-			for (int index = 0; index < allOrders.Length; index++) {
-				if ( allOrders[index] != null && allOrders[index] == execution.Order) {
+				if ( entryOrder != null && entryOrder == execution.Order) {
 					if (execution.Order.OrderState == OrderState.Filled || execution.Order.OrderState == OrderState.PartFilled || (execution.Order.OrderState == OrderState.Cancelled && execution.Order.Filled > 0)) {
 						
-						// We sum the quantities of each execution making up the entry order
+						/* We sum the quantities of each execution making up the entry order */
 						sumFilled += execution.Quantity;
 						Print("Sum of filled complete");
+						
 						/*Submit stop limit and long exit orders orders for partial fills
 						  These functions handle the entries for all of our long positions */
 						if (Position.MarketPosition == MarketPosition.Long) {
 							if (execution.Order.OrderState == OrderState.PartFilled) {
-								stopOrders[index] = ExitLongStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice - (stopTargetTicks[index, 0] * TickSize)), stopStrings[index], entryStrings[index]);
-								targetOrders[index] = ExitLongLimit(execution.Order.Filled, execution.Order.AverageFillPrice - (stopTargetTicks[index, 1] * TickSize), targetStrings[index], entryStrings[index]);
+								stopOrder = ExitLongStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice - (4 * TickSize)), "MyStop", "entry1");
+								targetOrder = ExitLongLimit(execution.Order.Filled, execution.Order.AverageFillPrice - (4 * TickSize), "MyTarget", "entry1");
 							}
 							//Update the exit order quantities once understate turns to filled and we have seen execution quantities match order quantities
 							else if (execution.Order.OrderState == OrderState.Filled && sumFilled == execution.Order.Filled) {
+								
 								//Stop Loss Order for OrderState.Filled
-								stopOrders[index] = ExitLongStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice - (stopTargetTicks[index, 0] * TickSize)), stopStrings[index], entryStrings[index]);
-								targetOrders[index] = ExitLongLimit(execution.Order.Filled, execution.Order.AverageFillPrice - (stopTargetTicks[index, 1] * TickSize), targetStrings[index], entryStrings[index]);
+								stopOrder = ExitLongStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice - (4 * TickSize)), "MyStop", "entry1");
+								targetOrder = ExitLongLimit(execution.Order.Filled, execution.Order.AverageFillPrice - (4 * TickSize), "MyTarget", "entry1");
 							}
 							//Reset the entryOrder object and the sumFilled counter to null / 0 after the order has been filled
 							if (execution.Order.OrderState != OrderState.PartFilled && sumFilled == execution.Order.Filled) {
-								allOrders[index] = null;
+								entryOrder = null;
 								sumFilled = 0;
 							}
 						}
@@ -281,39 +202,37 @@ namespace NinjaTrader.NinjaScript.Strategies
 						  These functions handle the entries for all of our short positions */
 						if (Position.MarketPosition == MarketPosition.Short) {
 							if (execution.Order.OrderState == OrderState.PartFilled) {
-								stopOrders[index] = ExitShortStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice + (stopTargetTicks[index, 0] * TickSize)), stopStrings[index], entryStrings[index]);
-								targetOrders[index] = ExitShortLimit(execution.Order.Filled, execution.Order.AverageFillPrice + (stopTargetTicks[index, 1] * TickSize), targetStrings[index], entryStrings[index]);
+								stopOrder = ExitLongStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice + (4 * TickSize)), "MyStop", "entry1");
+								targetOrder = ExitLongLimit(execution.Order.Filled, execution.Order.AverageFillPrice + (4 * TickSize), "MyTarget", "entry1");
 							}
 							//Update the exit order quantities once understate turns to filled and we have seen execution quantities match order quantities
 							else if (execution.Order.OrderState == OrderState.Filled && sumFilled == execution.Order.Filled) {
 								//Stop Loss Order for OrderState.Filled
-								stopOrders[index] = ExitShortStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice + (stopTargetTicks[index, 0] * TickSize)), stopStrings[index], entryStrings[index]);
-								targetOrders[index] = ExitShortLimit(execution.Order.Filled, execution.Order.AverageFillPrice + (stopTargetTicks[index, 1] * TickSize), targetStrings[index], entryStrings[index]);
+								stopOrder = ExitLongStopLimit(execution.Order.Filled, (execution.Order.AverageFillPrice + (4 * TickSize)), "MyStop", "entry1");
+								targetOrder = ExitLongLimit(execution.Order.Filled, execution.Order.AverageFillPrice + (4 * TickSize), "MyTarget", "entry1");
 							}
 							//Reset the entryOrder object and the sumFilled counter to null / 0 after the order has been filled
 							if (execution.Order.OrderState != OrderState.PartFilled && sumFilled == execution.Order.Filled) {
-								allOrders[index] = null;
+								entryOrder = null;
 								sumFilled = 0;
 							}
-						}
-						
-						
+						}	
 					}
 				}
 				
 				/* Reset the stop orders and target orders Order objects after our position has been closed */
-				if ((stopOrders[index] != null && stopOrders[index] == execution.Order) || (targetOrders[index] != null && targetOrders[index] == execution.Order)) {
+				if ((stopOrder != null && stopOrder == execution.Order) || (targetOrder != null && targetOrder == execution.Order)) {
 					if (execution.Order.OrderState != OrderState.PartFilled || execution.Order.OrderState == OrderState.PartFilled) {
-						stopOrders[index] = null;
-						targetOrders[index] = null;
+						stopOrder = null;
+						targetOrder = null;
 					}
 				}
 			}
-		}
 		
 	
 	
 	}
 }
 	
+
 
